4dbbf21f6f818f11fd79bc0e3e82f7c4183946bb
JUAN FELIPE JARAMILLO GALEANO <juanjaramillo223677@correo.itm.edu.co>
2020-06-07 19:32:08 -0500

    Core finish

---
+++ TipCalculator.Core/App.cs
@@ -0,0 +1,23 @@
+﻿using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TipCalculator.Core.ViewModels;
+
+namespace TipCalculator.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service") //Inyectar todo lo que termine en service
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<TipViewModel>(); // El proyecto arranca por esta clase
+        }
+    }
+
+}
